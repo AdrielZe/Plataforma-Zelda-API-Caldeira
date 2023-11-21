6eda22d042bc0643c4a2fcd096036e2ab307cd44
AdrielZe <oadriel14@gmail.com>
2023-11-21 20:09:37 -0300

    feat: adicionando modelo de usuário

--- src/main/java/com/zelda/zeldaprojeto/models/UserModel.java
+++ src/main/java/com/zelda/zeldaprojeto/models/UserModel.java
@@ -1,2 +1,25 @@
-package com.zelda.zeldaprojeto.models;public class UserModel {
+package com.zelda.zeldaprojeto.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+
+
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_user",nullable = false)
+    private Long id_user;
+    @Column(name="nome", nullable = false)
+    private String nome;
+    @Column(name="idade",nullable=false)
+    private int idade;
+
+    public void setId(Long id){
+        this.id_user = id;
+    }
+
 }
