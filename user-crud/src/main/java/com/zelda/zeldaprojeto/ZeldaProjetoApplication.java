9fa661fa9fe9ebb1f0e836f636fa19bc61bf57eb
AdrielZe <oadriel14@gmail.com>
2023-11-28 00:28:14 -0300

    Adicionando dependência Eureka ao User Crud

--- user-crud/src/main/java/com/zelda/zeldaprojeto/ZeldaProjetoApplication.java
+++ user-crud/src/main/java/com/zelda/zeldaprojeto/ZeldaProjetoApplication.java
@@ -3,6 +3,7 @@ package com.zelda.zeldaprojeto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ZeldaProjetoApplication {
 
