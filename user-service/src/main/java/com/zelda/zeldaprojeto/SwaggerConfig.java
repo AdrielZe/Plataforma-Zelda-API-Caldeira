a0cd810cfce2e49a3f42e975b75728d2bf9a1b6d
Lucas Vizeu <nota10.lucas@gmail.com>
2023-11-30 16:56:54 -0300

    1.0
    arrumando...

---
+++ user-service/src/main/java/com/zelda/zeldaprojeto/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.zelda.zeldaprojeto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.zelda.zeldaprojeto.v1"))
+                .paths(regex("(/merchant.*)"))
+                .build();
+    }
+}
--- user-service/src/main/java/com/zelda/zeldaprojeto/controllers/UserController.java
+++ user-service/src/main/java/com/zelda/zeldaprojeto/controllers/UserController.java
@@ -3,6 +3,9 @@ package com.zelda.zeldaprojeto.controllers;
 import com.zelda.zeldaprojeto.models.UserModel;
 import com.zelda.zeldaprojeto.repositories.UserRepository;
 import com.zelda.zeldaprojeto.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.apache.catalina.User;
+
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -10,12 +13,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
+
+
+//localhost:8080/swagger-ui.html
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping(value ="/usuarios", produces = {"application/json"})
+@Tag(name = "Zelda-Api")
+
 public class UserController {
 
     @Autowired
@@ -27,22 +33,53 @@ public class UserController {
     public ResponseEntity<List<UserModel>> acharTodosUsuarios(){
         return userService.acharTodosUsuarios();
     }
+    @Operation(summary = "Adiciona um usuario", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario adicionado com sucesso!!!"),
+            @ApiResponse(responseCode = "422", description = "Usuario de requisição inválido"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos"),
+
+    })
 
     @PostMapping(value = "/adicionar", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserModel> adicionarUsuario(@RequestBody UserModel user){
         return userService.adicionarUsuario(user);
     }
+    @Operation(summary = "Busca dados de usuario pelo seu nome", method = "GET")
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar busca dos dados"),
+    })
 
     @GetMapping("{id}")
     public ResponseEntity<List<UserModel>> buscarUsuario(@PathVariable Long id) {
         return
 userService.buscarUsuario(id);
     }
+    @Operation(summary = "Editar  usuario pelo seu id", method = "PUT")
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Edição realizada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao tentar busca dos dados"),
+    })
 
     @PutMapping(value = "{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserModel> editarUsuario(@PathVariable Long id, @RequestBody UserModel user){
        return userService.editarUsuario(id,user);
     }
+    @Operation(summary = "Deletar usuario pelo seu id", method = "DELETE")
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar busca dos dados"),
+    })
 
     @DeleteMapping("{id}")
     public void deletarUsuario(@PathVariable Long id){
