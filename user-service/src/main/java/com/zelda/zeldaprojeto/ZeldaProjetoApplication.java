68783e1bdcdf5210815585121d60c00ee0205ae8
AdrielZe <oadriel14@gmail.com>
2023-12-06 18:01:27 -0300

    nada alterado

--- user-service/src/main/java/com/zelda/zeldaprojeto/ZeldaProjetoApplication.java
+++ user-service/src/main/java/com/zelda/zeldaprojeto/ZeldaProjetoApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZeldaProjetoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ZeldaProjetoApplication.class, args);
 	}
-
 }
